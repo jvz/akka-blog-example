2c8684e237a724beafd23276fd7ee30921b944a0
Matt Sicker <matt.sicker@spr.com>
2017-07-03 12:31:53 -0500

    Add docs

--- src/main/scala/com/spr/blog/JsonSupport.scala
+++ src/main/scala/com/spr/blog/JsonSupport.scala
@@ -4,6 +4,6 @@ import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
 import io.circe.generic.AutoDerivation
 
 /**
-  *
+  * Mixin to provide automatic marshalling and unmarshalling support for case classes and various fundamental data types.
   */
 trait JsonSupport extends FailFastCirceSupport with AutoDerivation
